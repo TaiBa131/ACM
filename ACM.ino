b15c52f769e03d66ace4153d30281f963bf7a908
TaiBa131 <56110645+TaiBa131@users.noreply.github.com>
2020-01-14 09:08:13 +0100

    Cleaning up the code, removing not needed parts of POC

--- ACM.ino
+++ ACM.ino
@@ -18,7 +18,7 @@ void setup() {
   
   Serial.println("TareOffset: " + String(80)); // prints the Tare Offset to be saved later, as the device WILL be rebooted randomly when finished
   
-  sendMessage("Hello World without TTS AT ALL"); // Sends a test message via the webhook over to Discord
+  Serial.println("Hello World without TTS AT ALL"); // Sends a test message via the webhook over to Discord
 
 
 
@@ -42,10 +42,6 @@ void weigh() {
   checkWeight(i);
 }
 
-
-// Has kind of been tested
-// -> using paramater from weigh() to checkWeight() instead of global variable
-//
 // This should increase check, if the time interval was right and the "load" under 10g
 // If the check amount is equal to 5, it restes it, and prints something
 void checkWeight(float i) {
@@ -59,26 +55,6 @@ void checkWeight(float i) {
     prevTime = millis();
   }
 }
-
-
-
-void sendMessage(String content) {
-  Serial.println("[HTTP] Connecting to Discord...");
-  Serial.println("[HTTP] Message: " + content);
-  Serial.println("https://discordapp.com/webhook " "application/json " "{\"content\":\"" + content + "\"" + "}");
-  
-  int statusCode = 204;
-  
-  Serial.print("[HTTP] Status code: ");
-  Serial.println(statusCode);
-  
-  if(statusCode != 204) {
-    Serial.print("[HTTP] Response: ");
-    Serial.println("Error");
-  }
-}
-
-
 
 int LoadCellData() {
   return random(0, 11);
