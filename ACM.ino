a92a53af5a40eecfcf424d45a279e8a2423d2320
TaiBa131 <56110645+TaiBa131@users.noreply.github.com>
2020-01-17 13:48:13 +0100

    Going back to the Load Cell's data instead of randomized data + checking multiple times instead of using delay in loop

--- ACM.ino
+++ ACM.ino
@@ -1,6 +1,30 @@
 /*
-  ACM - Adaptive Classroom Manager - Early POC - Without Electronics
+  ACM - Adaptive Classroom Manager - Early POC$
+  This is currently "based" on the Ethernet library's DHCP-based IP printer sketch, @maditnerd implementation of sending Discord messages, aswell as Michael Schoeffler's OLED + HX711 code.
+  https://github.com/arduino-libraries/Ethernet/blob/master/examples/DhcpAddressPrinter/DhcpAddressPrinter.ino
+  https://github.com/maditnerd/discord_test/blob/master/discord_test_mkr1010/discord.h
+  http://www.mschoeffler.de/2017/12/04/arduino-tutorial-hx711-load-cell-amplifier-weight-sensor-module-lcm1602-iic-v1-lcd/
+  
+  Knowing pretty much nothing in electronics, arduino, the libraries i used, or even C++ (We ONLY cover JAVA at school), i looked up a lot of things online, and adapted the examples to fit my goal.
 */
+
+#include <Ethernet.h>
+#include <ArduinoHttpClient.h>
+  // HX711_ADC HAS A CHANGED config.h (#define SAMPLES 2)
+#include <HX711_ADC.h> // https://github.com/olkal/HX711_ADC
+#include "secrets.h"
+
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 // MAC address
+};
+
+const char server[] = SECRET_SERVER; // domain of my web server / reverse proxy
+const int port = 80; // HTTP + NGINX, because not powerful enough
+
+EthernetClient client;
+HttpClient http_client = HttpClient(client, server, port);
+
+HX711_ADC LoadCell(3, 4); // data pin + serial clock pin
 
 unsigned long prevTime= 0;
 unsigned long interval = 700;
@@ -14,6 +38,23 @@ void setup() {
     ; // wait for serial port to connect, needed for testing, otherwise its useless
   }
 
+  Serial.println("Getting IP address with DHCP");
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    while (true) {
+      delay(1);
+    }
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+
+  LoadCell.begin(); // start hx711 connection 
+  LoadCell.start(2000); // 2000 ms to stabilize but will be set to 0 when the tare offset is found
+  LoadCell.setCalFactor(4110.26); // Calibration factor from the calibration sketch
+
   randomSeed(analogRead(0)); // Random seed for Random number
 
   Serial.println("Setup successful.");
@@ -22,14 +63,21 @@ void setup() {
 
 
 void loop() {
+  Ethernet.maintain();
   weigh();
-  delay(500); // waits for a second and a half
+  // delay(500); // waits for a second and a half
 }
 
-
+void updateData() {
+  for (int tmp = 0; tmp < 16; tmp++) {
+    LoadCell.update();
+    delay(33);
+  }
+}
 
 void weigh() {
-  float i = LoadCellData();
+  updateData();
+  int i = LoadCell.getData();
   String text = "Weight[g]: " + String(i);
   Serial.print(text); // !!! Change to println !!!
 
@@ -42,7 +90,7 @@ void weigh() {
 // Otherwise, if its not under 10, or its not the interval YET,
 // it check if its over 10, and if its the case,
 // it resets it.
-void checkWeight(int i, String text) {
+void checkWeight(float i, String text) {
   if( (int(i) < 10) && ((unsigned long) (millis() - prevTime) >= interval)) {
     if(check==4) {
       check=0;
@@ -58,8 +106,4 @@ void checkWeight(int i, String text) {
   } else {                                                        //Not Needed
     Serial.println("");                                           //Not Needed
   }
-}
-
-int LoadCellData() {
-  return random(0, 11);
 }
