5dacb9be635f7c7c3948d123d9c034228db02f4a
TaiBa131 <56110645+TaiBa131@users.noreply.github.com>
2020-01-17 14:03:51 +0100

    Removing Serial print

--- ACM.ino
+++ ACM.ino
@@ -79,31 +79,20 @@ void weigh() {
   updateData();
   int i = LoadCell.getData();
   String text = "Weight[g]: " + String(i);
-  Serial.print(text); // !!! Change to println !!!
+  Serial.println(text);
 
   checkWeight(i, text);
 }
 
-// If the load is under 10g, its in the interval and if has successfully checked 4 times,
-// the Weight has been under 10 for 5 checks, so it sends a message and resets the check amount
-//
-// Otherwise, if its not under 10, or its not the interval YET,
-// it check if its over 10, and if its the case,
-// it resets it.
 void checkWeight(float i, String text) {
   if( (int(i) < 10) && ((unsigned long) (millis() - prevTime) >= interval)) {
     if(check==4) {
       check=0;
-      Serial.println("            Sending Message here");         //Not Needed
     } else {
       check++;
-      Serial.println(" (check++)  " + String(check) + "   / 5");  //Not Needed
       prevTime = millis();
     }
   } else if (int(i)>=10) {
     check=0;
-    Serial.println("           0     0");                         //Not Needed
-  } else {                                                        //Not Needed
-    Serial.println("");                                           //Not Needed
   }
 }
