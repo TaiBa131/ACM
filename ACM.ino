3371a05d109513ea6bfcd2dc80acdde9421cba5a
TaiBa131 <56110645+TaiBa131@users.noreply.github.com>
2020-01-28 00:10:16 +0100

    Cleaning up and switching to MQTT

--- ACM.ino
+++ ACM.ino
@@ -15,7 +15,6 @@ byte mac[] = {
 
 const char  broker[] = SECRET_SERVER;
 int         port = SECRET_PORT;
-
 String classroom = SECRET_CLASSROOM;
 String site = SECRET_SITE;
 
@@ -23,45 +22,19 @@ const int sensorPin = A5;
 const int VIN = 5;
 const int R = 10000;
 
+int sensorVal=0;
+int lux=0;
+
 EthernetClient client;
 MqttClient mqtt(client);
 HX711_ADC LoadCell(3, 4); // data pin + serial clock pin
 
-unsigned long prevTime= 0;
-unsigned long interval = 700;
-int sensorVal=0;
-int lux=0;
-int check=0;
-
 void setup() {
-
-  Serial.begin(9600); // Open serial with 9600 baud
+  beginSerial();
+  beginEthernet();
+  beginMqtt();
+  beginLoadCell();
   
-  while (!Serial) {
-    ; // wait for serial port to connect, needed for testing, otherwise its useless
-  }
-
-  Serial.println("Getting IP address with DHCP");
-
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    while (true) {
-      delay(1);
-    }
-  }
-
-  // print your local IP address:
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
-
-  if (!mqtt.connect(broker, port)) {
-    while (1);
-  }
-  
-  LoadCell.begin(); // start hx711 connection 
-  LoadCell.start(2000); // 2000 ms to stabilize but will be set to 0 when the tare offset is found
-  LoadCell.setCalFactor(4110.26); // Calibration factor from the calibration sketch
-
   Serial.println("Setup successful.");
 }
 
@@ -71,55 +44,25 @@ void loop() {
   mqtt.poll();
   Ethernet.maintain();
   
-  weigh();
-  // delay(500); // waits for a second and a half
+  weight();
+  light();
+  delay(3000);
 }
 
-void updateData() {
-  for (int tmp = 0; tmp < 16; tmp++) {
-    LoadCell.update();
-    delay(33);
-  }
+void weight() {
+  LoadCell.update();
+  int load = LoadCell.getData();
+  sendMessage(String(load), "box");
 }
 
-void weigh() {
-  updateData();
-  int i = LoadCell.getData();
-  String text = "Weight[g]: " + String(i);
-  Serial.println(text);
-
-  checkWeight(i, text);
-  checkLight();
-}
-
-void checkWeight(float i, String text) {
-  if( (int(i) < 10) && ((unsigned long) (millis() - prevTime) >= interval)) {
-    if(check==4) {
-      check=0;
-      sendMessage("0", "box"); // change later, testing
-    } else {
-      check++;
-      prevTime = millis();
-    }
-  } else if (int(i)>=10) {
-    check=0;
-  }
-}
-
-void checkLight() {
-  //Check if it is 3AM
-  int lumen = getLux();
-  if(lumen < 50){
-    Serial.print("Light Off    ");
-  } else {
-    Serial.print("Light On     ");
-  }
-  Serial.println(String(lumen));
+void light() {
+  sendMessage(String(getLux()), "light");
 }
 
 void sendMessage(String content, String reason){
   String topic = (site + "/classroom/" + classroom + "/" + reason);
   Serial.println(topic + ": \"" + content + "\"" );
+  Serial.println(content);
   mqtt.beginMessage(topic);
   mqtt.print(content);
   mqtt.endMessage();
@@ -131,4 +74,39 @@ int getLux() {
   float RLDR = (R * (VIN - Vout))/Vout; // Conversion voltage to resistance
   int phys=500/(RLDR/1000); // Conversion resitance to lumen
   return phys;
+}
+
+/////////////////////
+// SETUP FUNCTIONS //
+/////////////////////
+
+void beginSerial() {
+  Serial.begin(9600); // Open serial with 9600 baud
+  Serial.println("Serial is set up.");
+}
+
+void beginEthernet() {
+  Serial.println("Getting IP address with DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    while (true) {
+      delay(1);
+    }
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+}
+
+void beginMqtt() {
+  if (!mqtt.connect(broker, port)) {
+    while (1);
+  }
+}
+
+void beginLoadCell() {
+  LoadCell.begin(); // start hx711 connection 
+  LoadCell.start(2000); // 2000 ms to stabilize but will be set to 0 when the tare offset is found
+  LoadCell.setCalFactor(819); // Calibration factor from Serialthe calibration sketch
 }
