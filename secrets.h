54ed7e3e1020d7a290d3c5a9071e922d03e9c9fb
TaiBa131 <56110645+TaiBa131@users.noreply.github.com>
2020-01-28 16:35:52 +0100

    MQTT Broker

--- secrets.h
+++ secrets.h
@@ -1,9 +1,9 @@
 //Domain of the MQTT Server
-#define SECRET_SERVER     "maiou.root.sx"
+#define SECRET_SERVER     "test.mosquitto.org"
 //Port of the MQTT Server
 #define SECRET_PORT 1883
 
 //Classroom
-#define SECRET_CLASSROOM  "219"
+#define SECRET_CLASSROOM  "1"
 //Site of the Classroom
-#define SECRET_SITE "SL"
+#define SECRET_SITE "SITE"
